9ae328e0894bddb894bd6fa532b770a8fbfec064
stanislous <stanislousvanderputt@gmail.com>
2018-09-25 23:35:44 +0530

    Added Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RestaurantListComponent
   ],
   imports: [
     BrowserModule
