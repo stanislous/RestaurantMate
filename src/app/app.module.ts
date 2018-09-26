dfce3e9b3c98e4f9a0ef2cbd708e3257f43be538
stanislous <stanislousvanderputt@gmail.com>
2018-09-26 20:14:45 +0530

    added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RestaurantListComponent
+    RestaurantListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
